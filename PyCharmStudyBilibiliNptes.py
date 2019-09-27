7901b93c87733e6107310e10e6e4052105516b9d
kuiba1664 <48193533+kuiba1664@users.noreply.github.com>
2019-09-27 16:02:28 +0800

    py实验文件
    这是 一次py实验文件，第一次呦

---
+++ PyCharmStudyBilibiliNptes.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# author:Mingxuan Li
+# time:2019-09-24
+
+import tornado.web
+import urllib.request
+# 要养成代码洁癖的习惯
+
+
+def f():  # 函数上面要有2个空行
+    # TODO
+    for i in range(10):
+        print(i)
+
+
+f()
+# 上面这行代码的意思是，记录要做的事情；本软件左下角会显示出来TODO按钮，进入后点击，会跳到相应的TODO处；
+# response = urllib.request.urlopen('http://www.baidu.com')
+# 复制一行，Ctrl+D；
+# 删除一行，Ctrl+Y；
+# Ctrl+Z返回上一步，也就是撤销的意思；
+# 按Shift+回车键，快速换行换行;
+# 批量注释/批量取消注释：Ctrl+/；
+# 批量缩进：按Tab键；
+# 批量缩进倒退一层：Shift+Tab；
+# 查找：Ctrl+F，即find的意思，输入后会高亮显示；
+# 替换：Ctrl+R,即replace的意思；
+# 折叠代码：Ctrl+减号；
+# 展开代码：Ctrl+加号；
+# 折叠全部代码：Ctrl+Shift+减号；
+# 展开全部代码：Ctrl+Shift+加号；
+# 自定义快捷键：keymap；
+# 复制路径：选中*.py文件，Edit——Copy Path；
+# 打开文件所在文件夹：单击选中文件，右键，Show in Explorer；
+# 快速定位某模块，查看源文件：按住Ctrl，点击该单词或模块，就会立即跳出来；
+# 点击左侧Structure，显示结构。它会把其中包含的变量（由v代表）、类（由v代表）、方法（由f代表）；
+# 类中会包含方法。通过查看结构，可以查看别人源码特征。
+# 调试，Debug
+# 设置断点：在左侧行标号空白处点击，会出现红色圆坨坨；
+# 设置断点后，点击Debug后，每运行到这个地方，它就会暂停；
+# 点击Step Over,就会向下运行一步；再点击，会跳回设置断点出，并且会自动记录调试次数；
+# 断点的设置不是越多越好，而是在程序关键的地方设置，也可以选择其他按钮一次性运行出来；
+# 调试最考验的地方，就是断点的设置。
+
+
+
+
+
+
+
+
+
+
